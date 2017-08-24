38fc97d3a43a8d02adcd74009bc02fa2b2498668
Vishwaraj Bhattrai <vishwatek.sb67@gmail.com>
2017-08-24 23:24:53 +0530

    Update drozer_install.py

--- drozer_install.py
+++ drozer_install.py
@@ -1,8 +1,6 @@
 import os
 print "Installing Dependencies ..."
-
 print "\n"
-
 pwd=os.getcwd()
 os.system("wget wget https://pypi.python.org/packages/source/p/pyOpenSSL/pyOpenSSL-0.13.tar.gz")
 os.system("tar xzvf pyOpenSSL-0.13.tar.gz")
@@ -13,6 +11,7 @@ os.system("python setup.py build")
 os.system("python setup.py install")
 os.system("sudo easy_install --allow-hosts pypi.python.org protobuf==2.4.1")
 os.system("sudo easy_install twisted==10.2.0")
+print "\n"
 print "Installing drozer...."
 os.chdir(pwd)
 os.system("sudo easy_install ./drozer-2.3.4-py2.7.egg")
